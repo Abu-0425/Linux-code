519ef9eed95985e9f974bd1a19ff98b0235ba5e6
Abu-0425 <406307436@qq.com>
2021-06-07 15:30:18 +0800

    add mkfile/

---
+++ mkfile/a.c
@@ -0,0 +1,14 @@
+#include "a.h"
+
+void test()
+{
+    char *str = NULL;
+    strcpy(str, "hello world");
+}
+
+int main()
+{
+    print();
+    test();
+    return 0;
+}
---
+++ mkfile/b.c
@@ -0,0 +1,6 @@
+#include "a.h"
+
+void print()
+{
+    printf("hello world!\n");
+}
